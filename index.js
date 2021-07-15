ed1485967aaa14f894d95c0c78dbb251761e6677
fontes99 <pvfontes1000@gmail.com>
2021-07-15 12:44:43 -0300

    experiment

--- index.js
+++ index.js
@@ -64,36 +64,12 @@ async function monitorPrice() {
   try {
 
     // ADD YOUR CUSTOM TOKEN PAIRS HERE!!!
-
+    
     await checkPair({
       inputTokenSymbol: 'ETH',
       inputTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
       outputTokenSymbol: 'MKR',
       outputTokenAddress: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
-      inputAmount: web3.utils.toWei('1', 'ETHER')
-    })
-
-    await checkPair({
-      inputTokenSymbol: 'ETH',
-      inputTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-      outputTokenSymbol: 'DAI',
-      outputTokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
-      inputAmount: web3.utils.toWei('1', 'ETHER')
-    })
-
-    await checkPair({
-      inputTokenSymbol: 'ETH',
-      inputTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-      outputTokenSymbol: 'KNC',
-      outputTokenAddress: '0xdd974d5c2e2928dea5f71b9825b8b646686bd200',
-      inputAmount: web3.utils.toWei('1', 'ETHER')
-    })
-
-    await checkPair({
-      inputTokenSymbol: 'ETH',
-      inputTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-      outputTokenSymbol: 'LINK',
-      outputTokenAddress: '0x514910771af9ca656af840dff83e8264ecf986ca',
       inputAmount: web3.utils.toWei('1', 'ETHER')
     })
 
